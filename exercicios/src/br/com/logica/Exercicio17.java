470db376a5ef60affa4c2a8209ec89b3393957bb
Paulo-dev-98 <paulobg8@gmail.com>
2022-07-22 17:30:49 -0300

    exercicios ate o 18

---
+++ exercicios/src/br/com/logica/Exercicio17.java
@@ -0,0 +1,26 @@
+package br.com.logica;
+
+import java.util.Random;
+
+public class Exercicio17 {
+	public static void main(String[] args) {
+
+		Random aleatorio = new Random();
+
+		int quantidadeIntervalo = 0;
+
+		for (int i = 0; i < 80; i++) {
+			int numero = aleatorio.nextInt(500);
+			if (numero >= 10 && numero <= 150) {
+				quantidadeIntervalo++;
+				System.out.println("esse numero pertence ao grupro entre 10 e 150");
+			} else {
+				System.out.println("esse numero não pertence ao grupo entre 10 e 150");
+			}
+		}
+
+		System.out.println("dentro de 80 numero a quantidade que esta entre 10 e 150 é: " + quantidadeIntervalo);
+
+	}
+
+}
---
+++ exercicios/src/br/com/logica/Exercicio18.java
@@ -0,0 +1,33 @@
+package br.com.logica;
+
+import java.util.Random;
+
+public class Exercicio18 {
+
+	public static void main(String[] args) {
+		
+		int maiorIdade = 0;
+		int menorIdade = 0;
+		
+
+		Random aleatorio = new Random();
+
+		for (int i = 0; i <= 75; i++) {
+			int idade = aleatorio.nextInt(75);
+			String nome = "pessoa" + (i + 1);
+			if (idade < 18) {
+				menorIdade++;
+				System.out.println(nome + " é menor de idade");
+			} else {
+				maiorIdade++;
+				System.out.println(nome + " é maior de idade");
+			}
+          
+		}
+      
+		System.out.println(menorIdade + " pessoas são menores de idade\n"
+		+ maiorIdade + " pessoas são maiores de idade" );
+		
+	}
+
+}
