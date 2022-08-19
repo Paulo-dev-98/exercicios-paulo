005ad058f514c8d62e024c40ba4fcae4406d7659
Paulo-dev-98 <paulobg8@gmail.com>
2022-08-19 18:05:38 -0300

    iniciando o curso de poo capgemini

---
+++ POO Capgemini/src/br/com/poo1/Pessoa.java
@@ -0,0 +1,28 @@
+package br.com.poo1;
+
+public class Pessoa {
+
+	private float peso;
+	private float altura;
+	
+	public Pessoa(){
+		
+	}
+
+	public float getPeso() {
+		return peso;
+	}
+
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+
+	public float getAltura() {
+		return altura;
+	}
+
+	public void setAltura(float altura) {
+		this.altura = altura;
+	}
+	
+}
